0dde1532656a12f4c102795db83f2f8fa64a2ac1
jyoti1911sngwn <145776387+jyoti1911sngwn@users.noreply.github.com>
2024-12-25 10:14:05 -0800

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://express-backend-5lzy.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           'Content-Type': 'application/json',
