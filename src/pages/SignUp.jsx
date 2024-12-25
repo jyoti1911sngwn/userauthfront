c6ce774ec549c4ea596a7bd7c5d8c633b9d256c8
jyoti1911sngwn <145776387+jyoti1911sngwn@users.noreply.github.com>
2024-12-25 10:14:33 -0800

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -31,7 +31,7 @@ const SignUp = () => {
         return;
       }
     try {
-      const response = await fetch("http://localhost:5000/api/auth/createuser", {
+      const response = await fetch("https://express-backend-5lzy.onrender.com/api/auth/createuser", {
         method: "POST",
         headers: {
           'Content-Type': 'application/json',
